f07d7c6063ee937e860600c4cff148e8d345d11a
Jacob Della <jake.della4@gmail.com>
2024-08-08 15:11:49 -0400

    updated examples

--- example/README.md
+++ example/README.md
@@ -1,23 +1,22 @@
-# Proxmox/K3s Example
+# terraform-proxmox-k3s-multi-node
 
 This is an example project for setting up your own K3s cluster at home.
 
 ## Summary
 
 ### VMs
-
 This will spin up:
 
-- 1 support vm with api loadbalancer and k3s database with 2 cores and 8Gb mem
-- 1 master nodes with 2 cores and 8Gb mem
-- 2 node pool with 2 worker nodes each having 2 cores and 8Gb mem
+- 1 Support VM with API LoadBalancer and optionally MariaDB K3s database with 2 cores and 8GB of RAM. If embedded etcd only the LoadBalancer is deployed.
+- 3 master nodes spread across each PVE host with 2 cores and 2GB of RAM
+- 2 node pool with 2 worker nodes each having 8 cores and 10GB of RAM
 
 
 ### Networking
 
-- The support VM will be spun up on nodes `titan` using at `10.0.6.0`
-- The master VM will be spun up on nodes `titan` using at `10.0.6.1`
-- The worker VMs will be spun up on nodes `titan` at `10.0.6.8-10.0.6.23`
+- The support VM will be spun up on nodes `pve-prd0` using at `10.0.6.0`
+- The masters VMs will be spun up on nodes `pve-prd0`,`pve-prd1` and `pve-prd2` using at `10.10.2.1-10.10.1.3`
+- The masters VMs will be spun up on nodes `pve-prd0`,`pve-prd1` and `pve-prd2` using at `10.10.2.9,10.10.2.17,10.10.2.25`
 
 > Note: To eliminate potential IP clashing with existing computers on your
 network, it is **STRONGLY** recommended that you take IPs out of your DHCP server's rotation. Otherwise other computers
@@ -32,13 +31,13 @@ then
    should be able to do this by running
    `cat ~/.ssh/id_rsa.pub > authorized_keys`.
 2. Find your Proxmox API. It should look something like
-   `https://10.0.0.6:8006/api2/json`. Once you found it, update the value
+   `https://192.168.1.200:8006/api2/json`. Once you found it, update the value
    in the `main.tf` file marked as `TODO` in the `provider proxmox` section.
 3. Authenticate to the proxmox API **for the current terminal session** by setting the two variables:
-  ```
+  ```bash
   # Update these to be your proxmox user/password.
   # Note that you usually need to keep the @pam at the end of the user.
-  export PM_API_TOKEN_ID="root@pam!nytesolutions"
+  export PM_API_TOKEN_ID='root@pam!k3s'
   export PM_API_TOKEN_SECRET="something-something-something-something"
   ```
 
--- example/main.tf
+++ example/main.tf
@@ -1,127 +1,163 @@
-terraform{
+ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "2.9.3"
+      version = "3.0.1-rc3"
     }
 
     macaddress = {
       source  = "ivoronin/macaddress"
       version = "0.3.0"
-    }  
+    }
   }
 }
-
-
 
 provider "proxmox" {
   # make sure to export PM_API_TOKEN_ID and PM_API_TOKEN_SECRET
   pm_tls_insecure = true
-  pm_log_enable = true
-  pm_api_url      = "https://titan.local.com:8006/api2/json"
+  pm_log_enable   = true
+  pm_api_url      = "https://10.10.1.100:8006/api2/json"
+  pm_timeout      = 600
 }
 
 module "k3s" {
-    source  = "git::github.com/dellathefella/terraform-proxmox-k3s-multinode"
-    authorized_keys_file = "~/.ssh/id_rsa.pub"
-    authorized_private_key_file = "~/.ssh/id_rsa"
-    proxmox_node = "titan"
+  source                      = "git::github.com/dellathefella/terraform-proxmox-k3s-multinode"
+  authorized_keys_file        = "~/.ssh/id_rsa.pub"
+  authorized_private_key_file = "~/.ssh/id_rsa"
+  proxmox_node                = "pve-prd0"
 
-    #Support node if none specified installs onto entry point node
-    proxmox_support_node = "titan"
-    node_template = "ubuntu-2004-cloudinit-template"
-    network_gateway = "10.0.0.1"
-    lan_subnet = "10.0.0.1/18"
-    cluster_name = "dev"
-    support_node_settings = {
-        cores = 2
-        sockets = 1
-        memory = 8192
-        storage_type = "scsi"
-        storage_id   = "nytesolutions-fast-store"
-        disk_size    = "10G"
-        storage_type = "scsi"
-        user         = "support"
-        network_tag  = -1
-        db_name = "k3s"
-        db_user = "k3s"
-        network_bridge = "vmbr0"
-    }
+  #Support node if none specified installs onto entry point node
+  proxmox_support_node = "pve-prd0"
+  node_template        = "ubuntu-2204-cloudinit-template"
+  network_gateway      = "10.10.1.1"
+  lan_subnet           = "10.10.1.1/16"
+  cluster_name         = "jdella-com-prd"
+  # Enabling this setting disables the MariaDB support instance for the cluster.
+  # Changing this will trigger a cluster rebuild
+  # The main advantage of enabling embedded etcd is the cluster no longer has a single point of failure. But can increase resource usage.
+  cluster_enable_embedded_etcd = true
 
-    # Disable default traefik and servicelb installs for metallb and traefik 2
-    k3s_disable_components = [
-        "traefik",
-        "servicelb"
-    ]
-    # 10.0.6.1 - 10.0.6.6	(6 available IPs for nodes)
-    control_plane_subnet = "10.0.6.0/29"
+  support_node_settings = {
+    # DB related settings are ignored when cluster_enable_embedded_etcd = true
+    # If using embedded etcd the resources here should be dramatically reduced as Nginx is the main process running.
+    # Conversely the storage and specs for the control plane nodes should be increased.
+    cores        = 2
+    sockets      = 1
+    memory       = 1024
+    storage_type = "scsi"
+    storage_id   = "pve-ssd"
+    disk_size    = "10G"
+    storage_type = "scsi"
+    user         = "support"
+    network_tag  = -1
+    db_name        = "k3s"
+    db_user        = "k3s"
+    network_bridge = "vmbr0"
+  }
 
-    #This number must match the number of entries for master_nodes
-    #distributes the masters upon these nodes in sequential order if not specified the entry point node
-    master_node_target_nodes = ["titan"]
-    master_node_settings = {
-        cores          = 2
+  # Disable default traefik and servicelb installs for metallb and traefik 2
+  k3s_disable_components = [
+    "traefik",
+    "servicelb"
+  ]
+  # 10.10.2.1 - 10.10.2.6	(6 available IPs for nodes)
+  control_plane_subnet = "10.10.2.0/29"
+
+  # Distributes the masters upon these nodes in sequential order if not specified the entry point node
+  master_node_target_nodes = ["pve-prd0", "pve-prd1", "pve-prd2"]
+  master_node_settings = {
+    cores        = 2
+    sockets      = 1
+    memory       = 2048
+    storage_type = "scsi"
+    storage_id   = "pve-ssd"
+    user         = "k3s"
+    # Set disk_size much higher if using embedded etcd
+    disk_size      = "120G"
+    user           = "k3s"
+    network_bridge = "vmbr0"
+    network_tag    = -1
+    user           = "k3s"
+  }
+
+  node_pools = [
+    {
+      # 10.10.2.1 - 10.10.2.6	 (6 available IPs for nodes)
+      subnet = "10.10.2.8/29"
+
+      target_node = "pve-prd0"
+      size        = 1
+      node_pool_settings = {
+        name           = "pool0",
+        taints         = [""]
+        cores          = 8
         sockets        = 1
         memory         = 8192
         storage_type   = "scsi"
-        storage_id     = "local-lvm"
-        user           = "k3s"
-        disk_size      = "20G"
+        storage_id     = "pve-ssd"
+        disk_size      = "1000G"
         user           = "k3s"
         network_bridge = "vmbr0"
         network_tag    = -1
+        additonal_storage = {
+          storage_id = "pve-hdd"
+          disk_size  = "3500G"
+        }
+      }
+    },
+    {
+      # 10.10.2.17 - 10.10.2.22 (6 available IPs for nodes)
+      subnet = "10.10.2.16/29"
+
+      target_node = "pve-prd1"
+      size        = 1
+      node_pool_settings = {
+        name           = "pool1",
+        taints         = [""]
+        cores          = 8
+        sockets        = 1
+        memory         = 10240
+        storage_type   = "scsi"
+        storage_id     = "pve-ssd"
+        disk_size      = "1000G"
         user           = "k3s"
+        network_bridge = "vmbr0"
+        network_tag    = -1
+        additonal_storage = {
+          storage_id = "pve-hdd"
+          disk_size  = "3500G"
+        }
+      },
+    },
+    {
+      # 10.10.2.25 - 10.10.2.30 (6 available IPs for nodes)
+      subnet = "10.10.2.24/29"
+
+      target_node = "pve-prd2"
+      size        = 1
+      node_pool_settings = {
+        name           = "pool2",
+        taints         = [""]
+        cores          = 8
+        sockets        = 1
+        memory         = 10240
+        storage_type   = "scsi"
+        storage_id     = "pve-ssd"
+        disk_size      = "1000G"
+        user           = "k3s"
+        network_bridge = "vmbr0"
+        network_tag    = -1
+        additonal_storage = {
+          storage_id = "pve-hdd"
+          disk_size  = "3500G"
+        }
+      }
     }
-
-    # 192.168.0.200 -> 192.168.0.207 (6 available IPs for nodes)
-
-    node_pools = [
-        {
-        # 10.0.6.1 - 10.0.6.6	 (6 available IPs for nodes)
-        subnet = "10.0.6.8/29"
-
-        target_node = "titan"
-        size = 2
-        node_pool_settings = {
-          name           = "pool0",
-          taints         = [""]
-          cores          = 2
-          sockets        = 1
-          memory         = 8192
-          storage_type   = "scsi"
-          storage_id     = "nytesolutions-fast-store"
-          disk_size      = "20G"
-          user           = "k3s"
-          network_bridge = "vmbr0"
-          network_tag    = -1
-        }
-        },
-        {
-        # 10.0.6.16 - 10.0.6.23 (6 available IPs for nodes)
-        subnet = "10.0.6.16/29"
-
-        target_node = "titan"
-        size = 2
-        node_pool_settings = {
-          name           = "pool1",
-          taints         = [""]
-          cores          = 2
-          sockets        = 1
-          memory         = 8192
-          storage_type   = "scsi"
-          storage_id     = "nytesolutions-fast-store"
-          disk_size      = "20G"
-          user           = "k3s"
-          network_bridge = "vmbr0"
-          network_tag    = -1
-        }
-        }
-    ]
+  ]
 }
-
 
 output "kubeconfig" {
   # Update module name. Here we are using 'k3s'
-  value = module.k3s.k3s_kubeconfig
+  value     = module.k3s.k3s_kubeconfig
   sensitive = true
 }
